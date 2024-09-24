07ecec05ee408404d1bbef5dd4afa6fd820ca4a2
swathi <swathisherigar2003@gmail.com>
2024-09-24 10:59:04 +0530

    transition={{ duration: 2 }} to 1

--- src/Components/HeroTaxi.jsx
+++ src/Components/HeroTaxi.jsx
@@ -41,7 +41,7 @@ export default function HeroTaxi() {
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
-              transition={{ duration: 1 }}
+              transition={{ duration: 2 }}
               className="absolute inset-0"
             >
               <img
